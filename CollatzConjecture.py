4e799275c54f0d77c3435aae7daaa34ea6ae6aae
thiagobrito <69356402+othiagobrito@users.noreply.github.com>
2021-10-04 19:26:43 -0300

    Add files via upload

---
+++ CollatzConjecture.py
@@ -0,0 +1,21 @@
+number = 30
+step = 0
+n_list = []
+n_list.append(number)
+
+#3n+1
+
+while number != 1:
+
+    step = step + 1
+    if number % 2 == 0:
+        number = number / 2
+    else:
+        number = number * 3 + 1
+    
+    number = int(number)
+
+    n_list.append(number)
+
+    print(f'Step {step}\nNumber: {number}\n')
+print(f'Total Steps: {step}\nLog: {n_list}\nHighest number: {max(n_list)}')
